a18752b2223bf6af26935d6306cb54187ed622cc
mark199850 <mark199850@gmail.com>
2022-06-02 23:57:20 +0200

    top bar created, animated navigation, gallery card sample added

--- front-end/src/pages/Gallery.js
+++ front-end/src/pages/Gallery.js
@@ -1,8 +1,33 @@
 import React from 'react'
 
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 function Gallery() {
   return (
-    <div>Galéria</div>
+    <Card sx={{ maxWidth: 345 }}>
+    <CardMedia
+      component="img"
+      alt="green iguana"
+      height="140"
+      src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"    />
+    <CardContent>
+      <Typography gutterBottom variant="h5" component="div">
+        Cat
+      </Typography>
+      <Typography variant="body2" color="text.secondary">
+      The cat (Felis catus) is a domestic species of small carnivorous mammal. It is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family.
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Share</Button>
+      <Button size="small">Learn More</Button>
+    </CardActions>
+  </Card>
   )
 }
 
