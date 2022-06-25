e491865e952eb64cb063d9fb2af0ddf4419bc7bf
PapDani <papdaniel9977@gmail.com>
2022-06-25 20:53:01 +0200

    added under construction

--- src/App.js
+++ src/App.js
@@ -38,7 +38,24 @@ function App() {
 
   return (
 
-
+    <div>
+      <div style="padding: px; background-image: url(http://www.tourist-info.hu/fejlesztes-alatt.png); background-repeat: no-repeat; background-position: center; width: 100%; height: 100%;">
+        <div style="text-align: center; margin-top: 100px;">
+            <h1>Perfect System Hungary</h1>
+            <h2 style="">Elérhetőségek:</h2>
+            <div>
+                <h3 style="font-size: 22px;color: black;text-shadow: 1px 1px 10px white;">Telefonszám: +36202125022</h3>
+                <h3 style="font-size: 22px;color: black;text-shadow: 1px 1px 10px white;">Email cím: perfectsystemhungary@gmail.com</h3>
+            </div>
+    
+            <div>
+                <h1 style="margin-top: 300px;">Oldal fejlesztés alatt</h1>
+            </div>
+            
+        </div>
+      </div>
+    </div>
+/*
     <Box sx={{ flexGrow: 1}}>
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
@@ -92,6 +109,7 @@ function App() {
       </SwipeableViews>
     </Box>
     </Box>
+    */
   );
 }
 
@@ -110,6 +128,7 @@ function TabPanel(props) {
         <Box sx={{ p: 3 }}>
           <Typography component={'span'} /* default: p, causes warning */ >{children}</Typography>
         </Box>
+      
       )}
     </div>
   );
