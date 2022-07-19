14e1ad17f816ec9c98eac4cb62a75a4ea0a48415
PapDani <papdaniel9977@gmail.com>
2022-07-19 18:39:36 +0200

    state value

--- front-end/src/pages/Services.js
+++ front-end/src/pages/Services.js
@@ -1,22 +1,56 @@
 import React, { useState } from 'react';
-import {useForm } from "react-hook-form";
+
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 function Services(){
 
-  const {
-    register,
-    handleSubmit,
-    formState: {errors}
-  } = useForm({});
+  const[nevValue, setNevValue] = useState();
 
-  const onSubmit = () =>{
-
+  const nevValidate = () => {
+    alert(nevValue);
   }
-
 
   return(
     <div>
-      
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <div>
+          <TextField
+            id="1"
+            value={nevValue}
+            onChange={(e) => setNevValue(e.target.value)}
+          />
+          <TextField
+
+          />
+        </div>
+        <div>
+          <TextField
+
+          />
+          <TextField
+
+          />
+        </div>
+        <div>
+          <TextField
+
+          />
+          <TextField
+
+          />
+        </div>
+
+        <Button variant="contained" onClick={nevValidate()}>Küldés</Button>
+      </Box>
     </div>
   );
 }
