f53e80a177f842244308176495153533fc29e4ad
PapDani <papdaniel9977@gmail.com>
2022-07-20 17:00:14 +0200

    psuhing for co creator to solve regex problem in inputs

--- front-end/src/pages/Services.js
+++ front-end/src/pages/Services.js
@@ -4,52 +4,105 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
+import CheckCircleSharpIcon from '@mui/icons-material/CheckCircleSharp';
+
 function Services(){
 
-  const[nevValue, setNevValue] = useState();
+  const [nevValue, setNevValue] = useState('');
+  const handleNevChange = (event) => {
+    setNevValue(event.target.value);
+    nevError();
+  }
 
   const nevValidate = () => {
     alert(nevValue);
   }
 
+  const [nevErrorBoolean, setNevErrorBoolean] = useState(false);
+  const [nevErrorMsg, setNevErrorMsg] = useState('');
+  const [nevColor, setNevColor] = useState('primary')
+  const [nevVariant, setNevVariant] = useState("outlined");
+
+  const nevError = () => {
+    if(nevValue === undefined || nevValue === null || nevValue === " "){
+      setNevErrorBoolean(true);
+      setNevErrorMsg("Nem lehet üres ez a mező!")
+    }
+    else if(nevValue.length < 3){
+      setNevErrorBoolean(true);
+      setNevErrorMsg("Minimum 3 karakter hosszúságú legyen!")
+    }
+    else if(nevValue.length >= 25){
+      setNevErrorBoolean(true);
+      setNevErrorMsg("Maximum 25 karakter husszú lehet!")
+    }
+    else if(!/[^\w\s]/.test(nevValue)){
+      alert("spec karakter");
+      nevValue.replace(/[^\w\s]/gi, "");
+      setNevErrorBoolean(true);
+      setNevErrorMsg("Kérjük, ne használjon speciális karaktert!")
+    }
+    else{
+      setNevErrorBoolean(false);
+      setNevErrorMsg(<CheckCircleSharpIcon/>);
+      setNevColor('success');
+      setNevVariant('outlined');
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    nevValidate();
+    nevError();
+  }
+
   return(
     <div>
       <Box
-        component="form"
+        component=""
         sx={{
           '& .MuiTextField-root': { m: 1, width: '25ch' },
         }}
         noValidate
         autoComplete="off"
       >
-        <div>
-          <TextField
-            id="1"
-            value={nevValue}
-            onChange={(e) => setNevValue(e.target.value)}
-          />
-          <TextField
+        <form onSubmit={handleSubmit}>
+          <div>
+            <TextField
+              error={nevErrorBoolean}
+              id="1"
+              value={nevValue}
+              required
+              onChange={handleNevChange}
+              helperText={nevErrorMsg}
+              color={nevColor}
+              variant={nevVariant}
+            />
 
-          />
-        </div>
-        <div>
-          <TextField
+            <TextField
 
-          />
-          <TextField
+            />
+          </div>
+          <div>
+            <TextField
 
-          />
-        </div>
-        <div>
-          <TextField
+            />
+            <TextField
 
-          />
-          <TextField
+            />
+          </div>
+          <div>
+            <TextField
 
-          />
-        </div>
+            />
+            <TextField
 
-        <Button variant="contained" onClick={nevValidate()}>Küldés</Button>
+            />
+          </div>
+
+          <Button type='submit' variant="contained">Küldés</Button>
+
+        </form>
       </Box>
     </div>
   );
