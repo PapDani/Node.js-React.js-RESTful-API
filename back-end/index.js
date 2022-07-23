effb69604d53e1646051953c5784b739912a94cc
PapDani <papdaniel9977@gmail.com>
2022-07-23 08:42:58 +0200

    basic post works between frontend and backend

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,34 @@
 const express = require("express");
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"]
+}));
+
+app.use(express.json());
+
+app.post("/api/1", (req, res) => {
+  const name = req.body.name;
+  const mobil = req.body.mobil;
+  const email = req.body.email;
+  const leiras = req.body.leiras;
+
+  /*
+    if(!name === "" && !mobil === "" && !email === ""){
+      res.status(200).send({message: "Sikeres kommunikáció"});
+    }
+    else{
+      res.status(500).send({message: "Üresek, valami nem jó!"});
+    }
+  */
+
+    res.status(200).send({message: "Sikeres kommunikáció", name, mobil, email, leiras});
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
