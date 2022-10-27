304db7c9a5cf0273e5d609973be5f57ae002af2a
mark199850 <mark199850@gmail.com>
2022-10-27 20:00:38 +0200

    optimized responsivity for different sizes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,24 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
-import Contact from './pages/Contact';
-import Gallery from './pages/Gallery';
-import Services from './pages/Services';
-import Offer from './pages/Offer';
+import Contact from "./pages/Contact";
+import Gallery from "./pages/Gallery";
+import Services from "./pages/Services";
+import Offer from "./pages/Offer";
 
-import SwipeableViews from 'react-swipeable-views';
-import Box from '@mui/material/Box';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import { useTheme } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Grid from '@mui/material/Grid';
+import SwipeableViews from "react-swipeable-views";
+import Box from "@mui/material/Box";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import { useTheme } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Grid from "@mui/material/Grid";
 
-import { Email, Facebook, Call} from '@mui/icons-material';
+import { Email, Facebook, Call } from "@mui/icons-material";
 
 function App() {
-
   const theme = useTheme();
   const [value, setValue] = useState(0);
 
@@ -34,83 +33,85 @@ function App() {
   function a11yProps(index) {
     return {
       id: `full-width-tab-${index}`,
-      'aria-controls': `full-width-tabpanel-${index}`,
+      "aria-controls": `full-width-tabpanel-${index}`,
     };
   }
 
   return (
-
-
-  <Box>
-
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
-     
     <Box>
-    
-      <AppBar position="static" sx={{ backgroundColor:'#1f2d30'}}>
-        <Toolbar>
-        <Grid container spacing={2}>
-          <Grid item xs={12} lg={5}>
-            <Typography variant="h4" component="div">
-              Perfect System Hungary
-            </Typography>
-          </Grid>
-          <Grid item xs={12} sm={12} md={4} lg={3}>
-            <Email/>
-            <Typography variant="p" component="div">
-              <a href="mailto:perfectsystemhungary@gmail.com">perfectsystemhungary@gmail.com</a>
-            </Typography>
-          </Grid>
-          <Grid item xs={12} sm={12} md={4} lg={2}> 
-            <Call/>
-            <Typography variant="p" component="div">
-            <a href="tel:+36202125022">+36 (20) 212 5022</a>
-            </Typography>
-          </Grid>
-          <Grid item xs={12} sm={12} md={4} lg={1}>          
-            <Facebook/>
-            <Typography variant="p" component="div">
-              <a href='https://www.facebook.com/profile.php?id=100083219104359'>Facebook</a>
-            </Typography>
-          </Grid>
-        </Grid>
-        </Toolbar>
-      </AppBar>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
 
+      <Box>
+        <AppBar position="static" sx={{ backgroundColor: "#1f2d30" }}>
+          <Toolbar sx={{ padding: 1 }}>
+            <Grid container spacing={1}>
+              <Grid item xs={12} lg={5}>
+                <Typography variant="h4">Perfect System Hungary</Typography>
+              </Grid>
+              <Grid item xs={12} sm={12} md={4} lg={3}>
+                <Typography>
+                  <Email sx={{ marginBottom: -0.7 }} />
+                  <a href="mailto:perfectsystemhungary@gmail.com">
+                    perfectsystemhungary@gmail.com
+                  </a>
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={12} md={4} lg={2}>
+                <Typography>
+                  <Call sx={{ marginBottom: -0.7 }} />
+                  <a href="tel:+36202125022">+36 (20) 212 5022</a>
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={12} md={4} lg={2}>
+                <Typography /*variant="p" component="div"*/>
+                  <Facebook sx={{ marginBottom: -0.7 }} />
+                  <a href="https://www.facebook.com/profile.php?id=100083219104359">
+                    Facebook
+                  </a>
+                </Typography>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      <Box sx={{ width: "100%" }}>
+        <AppBar position="static" style={{ backgroundColor: "white" }}>
+          <Tabs value={value} onChange={handleChange} centered>
+            <Tab label="Ajánlat kérés" {...a11yProps(0)} />
+            <Tab label="Szolgáltatások" {...a11yProps(1)} />
+            <Tab disabled label="Galéria" {...a11yProps(2)} />
+            <Tab label="Kapcsolat" {...a11yProps(3)} />
+          </Tabs>
+        </AppBar>
+
+        <SwipeableViews
+          axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+          index={value}
+          onChangeIndex={handleChangeIndex}
+        >
+          <TabPanel value={value} index={0}>
+            <Offer />
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <Services sx={{ width: "100%", justifyContent: "center" }} />
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            <Gallery />
+          </TabPanel>
+          <TabPanel value={value} index={3} style={{ padding: 0 }}>
+            <Contact />
+          </TabPanel>
+        </SwipeableViews>
+      </Box>
     </Box>
-
-    <Box sx={{ width: '100%' }}>
-      <AppBar position="static" style={{backgroundColor: 'white'}}>
-        <Tabs value={value} onChange={handleChange} centered>
-        <Tab label="Ajánlat kérés"  {...a11yProps(0)} />
-          <Tab label="Szolgáltatások"  {...a11yProps(1)} />
-          <Tab disabled label="Galéria"  {...a11yProps(2)} />
-          <Tab label="Kapcsolat" {...a11yProps(3)}  />
-        </Tabs>
-      </AppBar>
-
-      <SwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={value}
-        onChangeIndex={handleChangeIndex}
-      >
-      <TabPanel value={value} index={0}>
-        <Offer />
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <Services />
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <Gallery />
-      </TabPanel>
-      <TabPanel value={value} index={3} style={{padding: 0}}>
-        <Contact />
-      </TabPanel>
-      </SwipeableViews>
-    </Box>
-
-  </Box>
   );
 }
 
@@ -127,7 +128,9 @@ function TabPanel(props) {
     >
       {value === index && (
         <Box sx={{ p: 3 }}>
-          <Typography component={'span'} /* default: p, causes warning */ >{children}</Typography>
+          <Typography component={"span"} /* default: p, causes warning */>
+            {children}
+          </Typography>
         </Box>
       )}
     </div>
--- front-end/src/pages/Services.js
+++ front-end/src/pages/Services.js
@@ -1,18 +1,18 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 import Gazlogo from "../images/Gaz.JPG";
 import Vizlogo from "../images/Viz.JPG";
 import Futeslogo from "../images/Futes.JPG";
 
 import "./Services.css";
 
-import Stack from '@mui/material/Stack';
+import Stack from "@mui/material/Stack";
 
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme({
   breakpoints: {
@@ -29,140 +29,187 @@ function Services() {
   return (
     <div>
       <ThemeProvider theme={theme}>
+        <Stack
+          direction={{ mobile: "column", laptop: "row" }}
+          spacing={{ mobile: 3, desktop: 6 }}
+          mb={3}
+          justifyContent="center"
+          alignItems="center"
+        >
+          {/* <Card sx={{ maxWidth: 400 }} lg={{maxWidth: 800}}> */}
+          <Card
+            sx={{
+              maxWidth: {
+                mobile: "100%",
+                tablet: 650,
+                laptop: 450,
+                desktop: 450,
+              },
+              width: {
+                mobile: "100%",
+                tablet: 650,
+                laptop: 450,
+                desktop: 450,
+              },
+            }}
+          >
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                // height='200'
+                sx={{
+                  height: {
+                    mobile: 250,
+                    tablet: 325,
+                    laptop: 225,
+                    desktop: 225,
+                  },
+                }}
+                src={Vizlogo}
+                alt="Vízszerelés logó"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Vízszerelés
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  sx={{
+                    height: {
+                      mobile: "auto",
+                      tablet: "auto",
+                      laptop: 225,
+                      desktop: 225,
+                    },
+                  }}
+                >
+                  Víz és lefolyórendszerek, szaniterek - mosdó,- mosogató, -
+                  zuhanyzó, - kád, - wc, - bojler szerelése, javítása, cseréje.
+                  Új víz és lefolyó csőhálózatok kiépítése, kialakítása,
+                  szaniterek beépítése. Vízhálózati rendszerelemek kiépítése,
+                  karbantartása, szervízelése.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
 
-      <Stack direction={{ mobile: 'column', laptop: 'row' }} spacing={{ mobile: 3, desktop: 6}} justifyContent="center" mb={3}>
-        {/* <Card sx={{ maxWidth: 400 }} lg={{maxWidth: 800}}> */}
-        <Card sx={{
-          width: {
-            mobile: 250,
-            tablet: 650,
-            laptop: 300,
-            desktop: 450
-          },
-          height: {
-            mobile: 250,
-            tablet: 650,
-            laptop: 300,
-            desktop: 450
-          }
-        }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              // height='200'
-              sx={{
-                height: {
-                  mobile: 125,
-                  tablet: 325,
-                  laptop: 150,
-                  desktop: 225
-                }
-              }}
-              src={Vizlogo}
-              alt="Vízszerelés logó"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Vízszerelés
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Víz és lefolyórendszerek, szaniterek - mosdó,- mosogató, - zuhanyzó, - kád, - wc, - bojler szerelése, javítása, cseréje.
-                Új víz és lefolyó csőhálózatok kiépítése, kialakítása, szaniterek beépítése. Vízhálózati rendszerelemek kiépítése, karbantartása, szervízelése.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
+          <Card
+            sx={{
+              maxWidth: {
+                mobile: "100%",
+                tablet: 650,
+                laptop: 450,
+                desktop: 450,
+              },
+              width: {
+                mobile: "100%",
+                tablet: 650,
+                laptop: 450,
+                desktop: 450,
+              },
+            }}
+          >
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                sx={{
+                  height: {
+                    mobile: 250,
+                    tablet: 325,
+                    laptop: 225,
+                    desktop: 225,
+                  },
+                }}
+                src={Futeslogo}
+                alt="green iguana"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Fűtésszerelés
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  sx={{
+                    height: {
+                      mobile: "auto",
+                      tablet: "auto",
+                      laptop: 225,
+                      desktop: 225,
+                    },
+                  }}
+                >
+                  Fűtésrendszer elemeinek: - radiátorok, - szelepek, - csapok, -
+                  szivattyúk és egyéb gépészeti elemek javítása, cseréje,
+                  kiépítése. Kazánok, fűtő készülékek javítása, szervízelése,
+                  karbantartása, tisztítása. Új fűtésrendszerek kiépítése,
+                  radiátoros, padlófűtés, fal és mennyezetfűtéses rendszerek
+                  kivitelezése, gépészeti és kéményrendszer kialakítása.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
 
-        <Card sx={{
-          width: {
-            mobile: 250,
-            tablet: 650,
-            laptop: 300,
-            desktop: 450
-          },
-          height: {
-            mobile: 250,
-            tablet: 650,
-            laptop: 300,
-            desktop: 450
-          }
-        }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              sx={{
-                height: {
-                  mobile: 125,
-                  tablet: 325,
-                  laptop: 150,
-                  desktop: 225
-                }
-              }}
-              src={Futeslogo}
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Fűtésszerelés
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Fűtésrendszer elemeinek: - radiátorok, - szelepek, - csapok, - szivattyúk és egyéb gépészeti elemek javítása, cseréje, kiépítése.
-                Kazánok, fűtő készülékek javítása, szervízelése, karbantartása, tisztítása.
-                Új fűtésrendszerek kiépítése, radiátoros, padlófűtés, fal és mennyezetfűtéses rendszerek kivitelezése, gépészeti és kéményrendszer kialakítása.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-
-        <Card sx={{
-          width: {
-            mobile: 250,
-            tablet: 650,
-            laptop: 300,
-            desktop: 450
-          },
-          height: {
-            mobile: 250,
-            tablet: 650,
-            laptop: 300,
-            desktop: 450
-          }
-        }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              sx={{
-                height: {
-                  mobile: 125,
-                  tablet: 325,
-                  laptop: 150,
-                  desktop: 225
-                }
-              }}
-              //image="C:/Users/papda/OneDrive/Asztali gép/Projekt01/Node.js-React.js-RESTful-API/front-end/images/Gáz.JPG"
-              //image='/front-end/src/pages/gaz.jpg'
-              //image='https://www.ops-store.fr/upload/image/replique-h-k-416-a5-black-umarex-vfc-gbbr-p-image-155396-grande.png'
-              src={Gazlogo}
-              alt="Gázszerelés logó"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Gázszerelés
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Gázcsőhálózatok, gázelzáró csapok, gázbekötő csövek cseréje, szerelése, korszerűsítése.
-                gázrendszerek tervezése terv ügyintézése, kivitelezése.
-                Gázkészülékek, vízemelegítők, kazánok javítása, szerelése, cseréje, karbantartáse.
-                Gáztűzhelyek garanciális beüzemelése.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </Stack>
-
+          <Card
+            sx={{
+              maxWidth: {
+                mobile: "100%",
+                tablet: 650,
+                laptop: 450,
+                desktop: 450,
+              },
+              width: {
+                mobile: "100%",
+                tablet: 650,
+                laptop: 450,
+                desktop: 450,
+              },
+            }}
+          >
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                sx={{
+                  height: {
+                    mobile: 250,
+                    tablet: 325,
+                    laptop: 225,
+                    desktop: 225,
+                  },
+                }}
+                src={Gazlogo}
+                alt="Gázszerelés logó"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Gázszerelés
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  sx={{
+                    height: {
+                      mobile: "auto",
+                      tablet: "auto",
+                      laptop: 225,
+                      desktop: 225,
+                    },
+                  }}
+                >
+                  Gázcsőhálózatok, gázelzáró csapok, gázbekötő csövek cseréje,
+                  szerelése, korszerűsítése. gázrendszerek tervezése terv
+                  ügyintézése, kivitelezése. Gázkészülékek, vízemelegítők,
+                  kazánok javítása, szerelése, cseréje, karbantartáse.
+                  Gáztűzhelyek garanciális beüzemelése.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Stack>
       </ThemeProvider>
     </div>
-  )
+  );
 }
 
-export default Services
+export default Services;
