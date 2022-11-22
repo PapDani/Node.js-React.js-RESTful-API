4dbf6e84f469fd7b09850339c2849d8d844e55f0
PapDani <papdaniel9977@gmail.com>
2022-11-22 15:55:16 +0100

    Services.js responsive!

--- front-end/src/pages/Services.js
+++ front-end/src/pages/Services.js
@@ -46,27 +46,35 @@ const theme = createTheme({
   },
 });
 
-var CardMaxWitdh_mobile = 100;
-var CardMaxWitdh_tablet = 100;
-var CardMaxWitdh_laptop = 100;
-var CardMaxWitdh_desktop = 100;
+var CardMaxWitdh_mobile = "100%";
+var CardMaxWitdh_tablet = "100%";
+var CardMaxWitdh_laptop = "100%";
+var CardMaxWitdh_desktop = "100%";
 
-var CardWidth_mobile = 100;
-var CardWidth_tablet = 100;
-var CardWidth_laptop = 100;
-var CardWidth_desktop = 100;
+var CardWidth_mobile = "100%";
+var CardWidth_tablet = "100%";
+var CardWidth_laptop = "100%";
+var CardWidth_desktop = "100%";
 
-var CardMediaHeight_mobile = 100;
-var CardMediaHeight_tablet = 100;
-var CardMediaHeight_laptop = 100;
-var CardMediaHeight_desktop = 100;
+var CardHeight_mobile = "100%";
+var CardHeight_tablet = "100%";
+var CardHeight_laptop = "100%";
+var CardHeight_desktop = "100%";
 
-var CardContentHeight_mobile = 100;
-var CardContentHeight_tablet = 100;
-var CardContentHeight_laptop = 100;
-var CardContentHeight_desktop = 100;
+var CardMediaHeight_mobile = "auto";
+var CardMediaHeight_tablet = "auto";
+var CardMediaHeight_laptop = "auto";
+var CardMediaHeight_desktop = "auto";
 
-const CardDataList = [
+var CardContentHeight_mobile = "auto";
+var CardContentHeight_tablet = "auto";
+var CardContentHeight_laptop = "auto";
+var CardContentHeight_desktop = "auto";
+
+var CardMediaMaxHeight_laptop = 250;
+var CardMediaMaxHeight_desktop = 300;
+
+const CardDataList1 = [
   {
     logo: Vizlogo,
     alt: "Vízszerelés logó",
@@ -76,7 +84,10 @@ const CardDataList = [
     logo: Futeslogo,
     alt: "Fűtésszerelés logó",
     typography: "Fűtésrendszer elemeinek: - radiátorok, - szelepek, - csapok, - szivattyúk és egyéb gépészeti elemek javítása, cseréje, kiépítése. Kazánok, fűtő készülékek javítása, szervízelése, karbantartása, tisztítása. Új fűtésrendszerek kiépítése, radiátoros, padlófűtés, fal és mennyezetfűtéses rendszerek kivitelezése, gépészeti és kéményrendszer kialakítása."
-  },
+  }
+]
+
+const CardDataList2 = [
   {
     logo: Gazlogo,
     alt: "Gázszerelés logó",
@@ -93,69 +104,160 @@ function Services() {
   return (
     <div>
       <ThemeProvider theme={theme}>
-        <Stack
-          direction={{ mobile: "column", laptop: "row" }}
-          spacing={{ mobile: 3, desktop: 3 }}
-          mb={3}
+        <Stack direction={{mobile: "column", desktop: "row" }}
+          spacing={{ mobile: 4, desktop: 4 }}
+          p={1}
           justifyContent="center"
-          alignItems="center"
         >
-          {CardDataList.map((val) => {
-            return (
-              /* <Card sx={{ maxWidth: 400 }} lg={{maxWidth: 800}}> */
-              <Card
-                sx={{ //miért jó, hogy van maxWidth is meg width is?
-                  maxWidth: {
-                    mobile: CardMaxWitdh_mobile,
-                    tablet: CardMaxWitdh_tablet,
-                    laptop: CardMaxWitdh_laptop,
-                    desktop: CardMaxWitdh_desktop
-                  },
-                  width: {
-                    mobile: CardWidth_mobile,
-                    tablet: CardWidth_tablet,
-                    laptop: CardWidth_laptop,
-                    desktop: CardWidth_desktop
-                  },
-                }}
-              >
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      height: {
-                        mobile: CardMediaHeight_mobile,
-                        tablet: CardMediaHeight_tablet,
-                        laptop: CardMediaHeight_laptop,
-                        desktop: CardMediaHeight_desktop
-                      },
-                    }}
-                    src={val.logo}
-                    alt={val.alt}
-                  />
-                  <CardContent
-                    sx={{
-                      height: {
-                        mobile: CardContentHeight_mobile,
-                        tablet: CardContentHeight_tablet,
-                        laptop: CardContentHeight_laptop,
-                        desktop: CardContentHeight_desktop
-                      },
-                    }}>
-                    <Typography
-                      variant="h6"
-                      color="text.secondary"
-                      textAlign="center"
-                      display="flex"
-                      alignItems="center"
-                    >
-                      {val.typography}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            )
-          })}
+          <Stack
+            direction={{ mobile: "column", tablet: "row" }}
+            spacing={{ mobile: 4, desktop: 4 }}
+            justifyContent="center"
+            width={{desktop: "50%"}}
+          //alignItems="center" - függőlegesen is középre teszi őket, ezért nem egy horizontális felős vonalhoz illeszkedve kezdődtek a Card-ok eddig
+          >
+            {CardDataList1.map((val) => {
+              return (
+                /* <Card sx={{ maxWidth: 400 }} lg={{maxWidth: 800}}> */
+                <Card
+                  sx={{ //miért jó, hogy van maxWidth is meg width is?
+                    maxWidth: {
+                      mobile: CardMaxWitdh_mobile,
+                      tablet: CardMaxWitdh_tablet,
+                      laptop: CardMaxWitdh_laptop,
+                      desktop: CardMaxWitdh_desktop
+                    },
+                    width: {
+                      mobile: CardWidth_mobile,
+                      tablet: CardWidth_tablet,
+                      laptop: CardWidth_laptop,
+                      desktop: CardWidth_desktop
+                    },
+                    height: {
+                      mobile: CardHeight_mobile,
+                      tablet: CardHeight_tablet,
+                      laptop: CardHeight_laptop,
+                      desktop: CardHeight_desktop
+                    }
+                  }}
+                >
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      sx={{
+                        maxHeight: {
+                          laptop: CardMediaMaxHeight_laptop,
+                          desktop: CardMediaMaxHeight_desktop
+                        },
+                        height: {
+                          mobile: CardMediaHeight_mobile,
+                          tablet: CardMediaHeight_tablet,
+                          laptop: CardMediaHeight_laptop,
+                          desktop: CardMediaHeight_desktop
+                        }
+                      }}
+                      src={val.logo}
+                      alt={val.alt}
+                    />
+                    <CardContent
+                      sx={{
+                        height: {
+                          mobile: CardContentHeight_mobile,
+                          tablet: CardContentHeight_tablet,
+                          laptop: CardContentHeight_laptop,
+                          desktop: CardContentHeight_desktop
+                        },
+                      }}>
+                      <Typography
+                        variant="h6"
+                        color="text.secondary"
+                        textAlign="center"
+                        display="flex"
+                        alignItems="center"
+                      >
+                        {val.typography}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              )
+            })}
+          </Stack>
+
+          <Stack
+            direction={{ mobile: "column", tablet: "row" }}
+            spacing={{ mobile: 4, desktop: 4 }}
+            justifyContent="center"
+            width={{desktop: "50%"}}
+          //alignItems="center" - függőlegesen is középre teszi őket, ezért nem egy horizontális felős vonalhoz illeszkedve kezdődtek a Card-ok eddig
+          >
+            {CardDataList2.map((val) => {
+              return (
+                /* <Card sx={{ maxWidth: 400 }} lg={{maxWidth: 800}}> */
+                <Card
+                  sx={{ //miért jó, hogy van maxWidth is meg width is?
+                    maxWidth: {
+                      mobile: CardMaxWitdh_mobile,
+                      tablet: CardMaxWitdh_tablet,
+                      laptop: CardMaxWitdh_laptop,
+                      desktop: CardMaxWitdh_desktop
+                    },
+                    width: {
+                      mobile: CardWidth_mobile,
+                      tablet: CardWidth_tablet,
+                      laptop: CardWidth_laptop,
+                      desktop: CardWidth_desktop
+                    },
+                    height: {
+                      mobile: CardHeight_mobile,
+                      tablet: CardHeight_tablet,
+                      laptop: CardHeight_laptop,
+                      desktop: CardHeight_desktop
+                    }
+                  }}
+                >
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      sx={{
+                        maxHeight: {
+                          laptop: CardMediaMaxHeight_laptop,
+                          desktop: CardMediaMaxHeight_desktop
+                        },
+                        height: {
+                          mobile: CardMediaHeight_mobile,
+                          tablet: CardMediaHeight_tablet,
+                          laptop: CardMediaHeight_laptop,
+                          desktop: CardMediaHeight_desktop
+                        },
+                      }}
+                      src={val.logo}
+                      alt={val.alt}
+                    />
+                    <CardContent
+                      sx={{
+                        height: {
+                          mobile: CardContentHeight_mobile,
+                          tablet: CardContentHeight_tablet,
+                          laptop: CardContentHeight_laptop,
+                          desktop: CardContentHeight_desktop
+                        },
+                      }}>
+                      <Typography
+                        variant="h6"
+                        color="text.secondary"
+                        textAlign="center"
+                        display="flex"
+                        alignItems="center"
+                      >
+                        {val.typography}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              )
+            })}
+          </Stack>
         </Stack>
       </ThemeProvider>
     </div>
